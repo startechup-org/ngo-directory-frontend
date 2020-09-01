e50c2a93eb529987439fc6c2984aa76947b1e70d
cleocredo <ccredo@startechup.com>
2020-09-01 13:53:09 +0800

    added login page and header

---
+++ src/components/header/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+    '@global': {
+      ul: {
+        margin: 0,
+        padding: 0,
+        listStyle: 'none',
+      },
+    },
+    appBar: {
+      borderBottom: `1px solid ${theme.palette.divider}`,
+    },
+    toolbar: {
+      flexWrap: 'wrap',
+    },
+    toolbarTitle: {
+      flexGrow: 1,
+    },
+    link: {
+      margin: theme.spacing(1, 1.5),
+    },
+}));
+
+export default function Pricing() {
+    const classes = useStyles();
+
+    return (
+        <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
+            <Toolbar className={classes.toolbar}>
+            <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
+                Company name
+            </Typography>
+            <nav>
+                <Link variant="button" color="textPrimary" href="#" className={classes.link}>
+                Features
+                </Link>
+                <Link variant="button" color="textPrimary" href="#" className={classes.link}>
+                Enterprise
+                </Link>
+                <Link variant="button" color="textPrimary" href="#" className={classes.link}>
+                Support
+                </Link>
+            </nav>
+            <Button href="#" color="primary" variant="outlined" className={classes.link}>
+                Login
+            </Button>
+            </Toolbar>
+        </AppBar>
+    )
+    
+}
