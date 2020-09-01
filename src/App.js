dc1cd1c6ab50a1e480d13aa0b668068bccea99a8
cleocredo <ccredo@startechup.com>
2020-09-01 16:42:54 +0800

    added recharts and admin dashboard

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import Header from './components/header/index';
 import LoginPage from './routes/login-page/view'
 import SignUpPage from './routes/signup-page/view'
 import NgoListPage from './routes/ngo-list-page/view'
+import AdminDashboard from './routes/admin-dashboard/view'
 
 function App() {
   return (
     <Router>
       <Header />
       <Switch>
-        <Route path="/signup" component={SignUpPage} />
+        <Route path="/admin" component={AdminDashboard} />
         <Route path="/list" component={NgoListPage} />
+        <Route path="/signup" component={SignUpPage} />
         <Route path="/" component={LoginPage} />
       </Switch>
     </Router>
