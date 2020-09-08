0253e961d251d9eb96d19855f051c1088e1daf29
cleocredo <ccredo@startechup.com>
2020-09-08 15:40:08 +0800

    integrate user signup api

--- src/routes/signup-page/view.js
+++ src/routes/signup-page/view.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
@@ -12,6 +12,8 @@ import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Grid from '@material-ui/core/Grid';
+
+import { user_signup } from '../../api/user.api'
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -36,6 +38,45 @@ const useStyles = makeStyles((theme) => ({
 export default function SignUp() {
   const classes = useStyles();
 
+  /* States */
+  const [username, setUsername] = useState('')
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [language, setLanguage] = useState('')
+  const [country, setCountry] = useState('')
+  const [ngoAdmin, setNgoAdmin] = useState('user')
+
+/* API Method */
+  const signup = async () => {
+    try {
+      const response = await user_signup({
+        username,
+        name,
+        email,
+        password,
+        language,
+        country,
+        ngoAdmin
+      });
+      console.log(response)
+    } catch (err) {
+      console.log('err login: ', err)
+    }
+  }
+
+  const handleSubmit = async (event) => {
+    console.log('test: ', event.currentTarget)
+    event.preventDefault();
+    const form = event.currentTarget;
+    console.log('form: ', form)
+    if (form.checkValidity() === false) {
+        event.stopPropagation();
+    } else {
+        signup()
+    }
+  };
+
   return (
     <React.Fragment>
       <CssBaseline />
@@ -47,59 +88,94 @@ export default function SignUp() {
             <Typography component="h1" variant="h5">
             Sign up
             </Typography>
-            <form className={classes.form} noValidate>
+            <form className={classes.form} noValidate onSubmit={handleSubmit}>
             <Grid container spacing={2}>
                 <Grid item xs={12} sm={6}>
-                <TextField
-                    autoComplete="fname"
-                    name="firstName"
-                    variant="outlined"
-                    required
-                    fullWidth
-                    id="firstName"
-                    label="First Name"
-                    autoFocus
-                />
+                  <TextField
+                      autoComplete="uname"
+                      name="username"
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="usename"
+                      label="Username"
+                      autoFocus
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                  />
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                <TextField
-                    variant="outlined"
-                    required
-                    fullWidth
-                    id="lastName"
-                    label="Last Name"
-                    name="lastName"
-                    autoComplete="lname"
-                />
+                  <TextField
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="fullname"
+                      label="Full Name"
+                      name="fullName"
+                      autoComplete="fname"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                  />
                 </Grid>
                 <Grid item xs={12}>
-                <TextField
-                    variant="outlined"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                />
+                  <TextField
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="email"
+                      label="Email Address"
+                      name="email"
+                      autoComplete="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                  />
                 </Grid>
                 <Grid item xs={12}>
-                <TextField
-                    variant="outlined"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                />
+                  <TextField
+                      variant="outlined"
+                      required
+                      fullWidth
+                      name="password"
+                      label="Password"
+                      type="password"
+                      id="password"
+                      autoComplete="current-password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                  />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                  <TextField
+                      autoComplete="lang"
+                      name="language"
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="language"
+                      label="Language"
+                      autoFocus
+                      value={language}
+                      onChange={(e) => setLanguage(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                      variant="outlined"
+                      required
+                      fullWidth
+                      id="country"
+                      label="Country"
+                      name="country"
+                      autoComplete="country"
+                      value={country}
+                      onChange={(e) => setCountry(e.target.value)}
+                  />
                 </Grid>
                 <Grid item xs={12}>
-                <FormControlLabel
-                    control={<Checkbox value="allowExtraEmails" color="primary" />}
-                    label="I want to receive inspiration, marketing promotions and updates via email."
-                />
+                  <FormControlLabel
+                  control={<Checkbox value={ngoAdmin} onChange={() => setNgoAdmin('ngo_admin')} color="primary" />}
+                      label="I am an NGO Admin"
+                  />
                 </Grid>
             </Grid>
             <Button
