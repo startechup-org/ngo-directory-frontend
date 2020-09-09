c2797a97a1cdbd9d97db7e78d991d1ab0df8d8fd
cleocredo <ccredo@startechup.com>
2020-09-09 20:29:18 +0800

    added conditional rendering if no managed organization

--- src/routes/ngo-list-page/view.js
+++ src/routes/ngo-list-page/view.js
@@ -43,9 +43,11 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Album() {
   const classes = useStyles();
+  const user_id = localStorage.getItem('user_id')
 
   /* States */
   const [organizations, setOrganizations] = useState([]);
+  const [managedOrganizations, setManagedOrganizations] = useState([]);
 
   // const loadOrganizations = async () => {
   //   const response = await allOrganizations();
@@ -54,6 +56,15 @@ export default function Album() {
   // }
 
   // useEffect(loadOrganizations(), [])
+
+  useEffect(() => {
+    const  loadManagedOrgs = async () => {
+      const response = await managedOrganizationsByUser(user_id);
+      console.log('response: ', response.data.data)
+      setManagedOrganizations(response.data.data)
+    }
+    loadManagedOrgs()
+  }, [])
 
   useEffect(() => {
     const loadOrganizations = async () => {
@@ -76,42 +87,46 @@ export default function Album() {
             </Typography>
           </Container>
         </div>
-        {/* Managed Organizations */}
-        <Container className={classes.cardGrid} maxWidth="md">
-          <Typography variant="h5" align="left" color="textPrimary" gutterBottom>
-            Organizations you manage
-          </Typography>
-          <br></br>
-          <Grid container spacing={4}>
-            {organizations.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {card.org_name}
-                    </Typography>
-                    <Typography>
-                      {card.org_description}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      View
-                    </Button>
-                    <Button size="small" color="primary">
-                      Edit
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
+         {/* Managed Organizations */}
+        {managedOrganizations.length > 0 && (
+          <Container className={classes.cardGrid} maxWidth="md">
+            <Typography variant="h5" align="left" color="textPrimary" gutterBottom>
+              Organizations you manage
+            </Typography>
+            <br></br>
+            <Grid container spacing={4}>
+              {managedOrganizations.map((card) => (
+                <Grid item key={card} xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image="https://source.unsplash.com/random"
+                      title="Image title"
+                    />
+                    <CardContent className={classes.cardContent}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {card.org_name}
+                      </Typography>
+                      <Typography>
+                        {card.org_description}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small" color="primary">
+                        View
+                      </Button>
+                      <Button size="small" color="primary">
+                        Edit
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        )}
+       
+
         {/* End Managed Organizations */}
         {/* Other Organizations */}
         <Container className={classes.cardGrid} maxWidth="md">
