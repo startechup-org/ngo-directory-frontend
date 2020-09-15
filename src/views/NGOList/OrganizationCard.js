142b6b1fedec10f2e131a890f4a5ffcda4de5066
cleocredo <ccredo@startechup.com>
2020-09-15 21:26:05 +0800

    added modal

--- src/views/NGOList/OrganizationCard.js
+++ src/views/NGOList/OrganizationCard.js
@@ -9,33 +9,49 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 
+import ViewOrganization from "./ViewOrganization"
+
 const OrganizationCard = (card) => {
   const classes = useStyles();
 
+  const [open, setOpen] = React.useState(false);
+
+	const handleClickOpen = () => {
+	  setOpen(true);
+	};
+  
+  const handleClose = () => {
+	  setOpen(false);
+	};
+
   return (
-    <Grid item key={card} xs={12} sm={6} md={4}>
-      <Card className={classes.card}>
-        <CardMedia
-          className={classes.cardMedia}
-          image="https://source.unsplash.com/random"
-          title="Image title"
-        />
-        <CardContent className={classes.cardContent}>
-          <Typography gutterBottom variant="h5" component="h2">
-            {card.org_name}
-          </Typography>
-          <Typography>{card.org_description}</Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" color="primary">
-            View
-          </Button>
-          <Button size="small" color="primary">
-            Edit
-          </Button>
-        </CardActions>
-      </Card>
-    </Grid>
+    <>
+      <Grid item key={card} xs={12} sm={6} md={4}>
+        <Card className={classes.card}>
+          <CardMedia
+            className={classes.cardMedia}
+            image="https://source.unsplash.com/random"
+            title="Image title"
+          />
+          <CardContent className={classes.cardContent}>
+            <Typography gutterBottom variant="h5" component="h2">
+              {card.org_name}
+            </Typography>
+            <Typography>{card.org_description}</Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" color="primary" onClick={handleClickOpen}>
+              View
+            </Button>
+            <Button size="small" color="primary">
+              Edit
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
+      <ViewOrganization open={open} close={handleClose} aria-labelledby="form-dialog-title"/>
+    </>
+   
   );
 };
 
