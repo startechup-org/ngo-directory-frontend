b1733be81ea8702ffb5f7931af04d29d9a2d3536
cleocredo <ccredo@startechup.com>
2020-09-16 15:10:23 +0800

    added api for get org by id

--- src/api/organization.api.js
+++ src/api/organization.api.js
@@ -3,3 +3,7 @@ import api from 'utils/api';
 export function allOrganizations() { 
     return api.get('/organizations');
 }
+
+export function organizationById(organization_id) { 
+    return api.get(`/organization/${organization_id}`);
+}
